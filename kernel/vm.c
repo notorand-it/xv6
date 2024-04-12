9828d53da5c7a7941304e0275724cd493378fdd9
Damilola Ajiboye <ajibigad@gmail.com>
2024-04-12 09:48:39 +0100

    complete print page table task in page table lab

--- kernel/vm.c
+++ kernel/vm.c
@@ -345,7 +345,7 @@ uvmclear(pagetable_t pagetable, uint64 va)
   pte = walk(pagetable, va, 0);
   if(pte == 0)
     panic("uvmclear");
-  *pte &= ~PTE_U;
+  *pte &= ~PTE_U; //clear the user bit flag so this page cannot be accessed in user mode
 }
 
 // Copy from kernel to user.
@@ -443,4 +443,27 @@ copyinstr(pagetable_t pagetable, char *dst, uint64 srcva, uint64 max)
   } else {
     return -1;
   }
+}
+
+static void printwalk(pagetable_t pagetable, int level) {
+  char* prefix[3] = {".. .. ..", ".. ..", ".."};
+
+  // there are 2^9 = 512 PTEs in a page table.
+  for(int i = 0; i < 512; i++){
+    pte_t pte = pagetable[i];
+    if(pte & PTE_V){
+      printf("%s%d: pte %p pa %p\n", prefix[level], i, pte, PTE2PA(pte));
+
+      if((pte & (PTE_R|PTE_W|PTE_X)) == 0) {
+        // this PTE points to a lower-level page table.
+        uint64 child = PTE2PA(pte);
+        printwalk((pagetable_t)child, level - 1);
+      }
+    }
+  }
+}
+
+void vmprint(pagetable_t pagetable) {
+  printf("page table %p\n", pagetable);
+  printwalk(pagetable, 2);
 }
