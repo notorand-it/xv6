8be39614b8d556cda85f383a6638e8b2804da68d
TornaxO7 <tornax@proton.me>
2024-04-22 20:41:52 +0200

    adding flake

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { nixpkgs, ... }:
+    let
+      system = "x86_64-linux";
+
+      riscv-pkgs = import nixpkgs {
+        inherit system;
+
+        crossSystem = {
+          config = "riscv64-unknown-linux-gnu";
+        };
+      };
+
+      pkgs = import nixpkgs {
+        inherit system;
+      };
+    in
+    {
+      devShells.${system}.default = riscv-pkgs.mkShell {
+        packages = with riscv-pkgs; [
+          libgcc
+        ] ++ (with pkgs; [
+          gnumake
+          qemu
+        ]);
+      };
+    };
+}
