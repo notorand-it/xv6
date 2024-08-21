17b9d4942bcb2c824447f03e590fe4647f7b5ed1
nate <jmy3092@autuni.ac.nz>
2024-08-21 21:23:43 +1200

    add working imports to file

--- user/ps.c
+++ user/ps.c
@@ -2,6 +2,12 @@
 #include "kernel/stat.h"
 #include "user/user.h"
 
+#include "kernel/param.h"
+#include "kernel/memlayout.h"
+#include "kernel/riscv.h"
+#include "kernel/spinlock.h"
+#include "kernel/proc.h"
+
 int main(int argc, char *argv[])
 {
   printf("Hello from my test case in xv6\n");
