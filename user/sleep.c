63502d104f340a18affcc1031630b23c862ea9f8
lee <leejohnny2024@gmail.com>
2024-09-13 23:48:08 +0800

    init

---
+++ user/sleep.c
@@ -0,0 +1,12 @@
+#include"kernel/types.h"
+#include"user.h"
+int main(int argc, char*argv[]){
+    if(argc != 2){
+        printf("Sleep needs one argument!\n"); //检查参数数里是否正确
+        exit(-1);
+    }
+    int ticks=atoi(argv[1]);//f将字符串参数转为整数
+    sleep(ticks);//使用系统调用s1eep 
+    printf("(nothing happens for a little while)\n");
+    exit(0);//确保进程退出
+}
