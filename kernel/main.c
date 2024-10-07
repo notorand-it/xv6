9bcc35534574c6fb0396016b075ae7cf139bc8c3
Markus Werner <markus@echt-werner.de>
2024-10-07 14:19:55 +0200

    fix: hartid 0 was not shown in the Boot prompt

--- kernel/main.c
+++ kernel/main.c
@@ -29,6 +29,8 @@ main()
     fileinit();      // file table
     virtio_disk_init(); // emulated hard disk
     userinit();      // first user process
+    printf("\n");
+    printf("hart %d started\n", cpuid());
     __sync_synchronize();
     started = 1;
   } else {
