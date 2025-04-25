eaf6220b59a246dbda82592afa95452010c804cc
Vincenzo Romano <vincenzo.romano@gmail.com>
2025-04-25 08:46:20 +0200

    kstr

--- kernel/types.h
+++ kernel/types.h
@@ -8,3 +8,12 @@ typedef unsigned int  uint32;
 typedef unsigned long uint64;
 
 typedef uint64 pde_t;
+
+// Kernel (compact) string;
+typedef struct {
+  union {
+    uint64 u64;
+    uchar  uc[sizeof(uint64)];
+  };
+  uchar eos[0];
+} kstr;
