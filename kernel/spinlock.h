88bc77aa96e9507b47f1bc53f778ffdfd49575e2
Vincenzo Romano <vincenzo.romano@gmail.com>
2025-04-26 15:32:53 +0200

    header

--- kernel/spinlock.h
+++ kernel/spinlock.h
@@ -2,10 +2,7 @@
 #include "types.h"
 
 // Mutual exclusion lock.
-struct spinlock {
-  uint locked;       // Is the lock held?
-
-  // For debugging:
-  char *name;        // Name of lock.
-  struct cpu *cpu;   // The cpu holding the lock.
-};
+typedef struct {
+  uint64 lock; // Locking data: MSB flag + mhartid
+  uint64 name; // Debugging 8-char string
+} spinlock;
